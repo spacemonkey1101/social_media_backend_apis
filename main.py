a83185963113e9f72996656458cffbdb135103a1
spacemonkey1101 <ritambh54@gmail.com>
2023-09-02 15:05:07 +0530

    comment on decorator

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI() #fastapi instance
 
-@app.get("/")
-def get_root():
+@app.get("/")   # GET operation on ROOT path/route -- this does the fastapi magic
+def get_root(): 
     return {"Data" : "hello world"}

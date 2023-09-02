3889a3638a03c25ad06f026310104e55e93a1839
spacemonkey1101 <ritambh54@gmail.com>
2023-09-02 16:46:34 +0530

    schema definition and usage in create post

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI() #fastapi instance
+
+# schema of post
+class Post(BaseModel):
+    title : str
+    content : str
 
 @app.get("/")   # GET operation on ROOT path/route -- this does the fastapi magic
 def get_root(): 
@@ -11,5 +17,5 @@ def get_posts():
     return {"Data" : "Get Posts"}
 
 @app.post("/create-post")
-def create_post(payload : dict = Body(...)):
-    return {"New Post" : f"Title : {payload['title']} Content: {payload['content']}"}
+def create_post(new_post : Post ):
+    return {"New Post" : f"Title : {new_post.title} Content: {new_post.content}"}

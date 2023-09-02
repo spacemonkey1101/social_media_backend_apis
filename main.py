7eabaee14083ab0ec87f62efbf861d7e75773e79
spacemonkey1101 <ritambh54@gmail.com>
2023-09-02 16:47:26 +0530

    format code

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 
-app = FastAPI() #fastapi instance
+app = FastAPI()  # fastapi instance
 
 # schema of post
 class Post(BaseModel):
-    title : str
-    content : str
+    title: str
+    content: str
 
-@app.get("/")   # GET operation on ROOT path/route -- this does the fastapi magic
-def get_root(): 
-    return {"Data" : "hello world"}
+
+@app.get("/")  # GET operation on ROOT path/route -- this does the fastapi magic
+def get_root():
+    return {"Data": "hello world"}
+
 
 @app.get("/posts")
 def get_posts():
-    return {"Data" : "Get Posts"}
+    return {"Data": "Get Posts"}
+
 
 @app.post("/create-post")
-def create_post(new_post : Post ):
-    return {"New Post" : f"Title : {new_post.title} Content: {new_post.content}"}
+def create_post(new_post: Post):
+    return {"New Post": f"Title : {new_post.title} Content: {new_post.content}"}

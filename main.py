fafa7a7e42d161ac47f69a95e3465058130c1bf4
spacemonkey1101 <ritambh54@gmail.com>
2023-09-02 16:05:30 +0530

    add a new GET request

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI() #fastapi instance
 @app.get("/")   # GET operation on ROOT path/route -- this does the fastapi magic
 def get_root(): 
     return {"Data" : "hello world"}
+
+@app.get("/posts")
+def get_posts():
+    return {"Data" : "Get Posts"}

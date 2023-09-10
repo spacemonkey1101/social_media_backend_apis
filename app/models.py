3d881e02d586ef1588b5261af7065a99781a3a93
spacemonkey1101 <ritambh54@gmail.com>
2023-09-10 16:37:45 +0530

    add columns to model like columns to table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,11 @@
+from sqlalchemy import Boolean, Column, Integer, String
 from .database import Base
 
+#   Post represents our postgres table 'posts' 
 class Post(Base):
     __tablename__ = 'posts'
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String,nullable=False)
+    published = Column(Boolean, default=True) 

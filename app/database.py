e8d07d6f2e7551ebd2b6215d933968a1e7b44fc2
spacemonkey1101 <ritambh54@gmail.com>
2023-09-10 16:25:44 +0530

    Add Base class for database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+# dont worry; dont need to understand everything in depth;
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@<postgresserver/ip>/db"
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password123@localhost/social_media_db"
 
@@ -9,3 +9,4 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()

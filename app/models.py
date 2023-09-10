1db420e7186aa2830528ac423880aa4860d96700
spacemonkey1101 <ritambh54@gmail.com>
2023-09-10 19:25:53 +0530

    passing string to server_default

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from .database import Base
 
-#   Post represents our postgres table 'posts' 
+
+#   Post represents our postgres table 'posts'
 class Post(Base):
-    __tablename__ = 'posts'
-    
+    __tablename__ = "posts"
+
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
-    content = Column(String,nullable=False)
-    published = Column(Boolean, server_default=True) 
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default="TRUE")

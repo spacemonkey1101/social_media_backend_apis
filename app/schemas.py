a18c2118c86836cc284e17634344b4c94370e1c6
spacemonkey1101 <ritambh54@gmail.com>
2023-09-17 16:41:19 +0530

    remove UserLogin Schema as we use OAuth2PasswordRequestForm now

--- app/schemas.py
+++ app/schemas.py
@@ -37,6 +37,6 @@ class UserResponse(BaseModel):
         orm_mode = True
 
 
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
+# class UserLogin(BaseModel):
+#     email: EmailStr
+#     password: str

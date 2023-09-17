1c8e4c99ef6b1d164ce9e84848f53359f6ed9773
spacemonkey1101 <ritambh54@gmail.com>
2023-09-17 16:15:44 +0530

    return JWT token from login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, FastAPI, status, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 
 router = APIRouter(prefix="/login", tags=["Auth"])
 
@@ -17,10 +17,13 @@ def user_login(user_login_info: schemas.UserLogin, db: Session = Depends(get_db)
             detail="Invalid Credentials",
         )
     verified_pass = utils.verify_hashed_password(
-        user_login_info.password, user_info_from_db.password)
-    if verified_pass:
+        user_login_info.password, user_info_from_db.password
+    )
+    if not verified_pass:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Invalid Credentials",
         )
-    return {"details": "login failed"}
+    # get a new JWT token
+    token = oauth2.create_access_token(payload={"user_id": user_info_from_db.id})
+    return {"access_token": token, "token_type": "bearer"}

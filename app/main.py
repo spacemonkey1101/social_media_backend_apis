146926d0151708f57445849c219315c14f966e2d
spacemonkey1101 <ritambh54@gmail.com>
2023-09-17 14:44:57 +0530

    add auth router to main app

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import Depends, FastAPI
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()  # fastapi instance
-app.include_router(post.router) # adding router to app object
-app.include_router(user.router) # adding router to app object
+app.include_router(post.router)  # adding router to app object
+app.include_router(user.router)  # adding router to app object
+app.include_router(auth.router)
+
 
 @app.get("/")  # GET operation on ROOT path/route -- this does the fastapi magic
 def get_root():

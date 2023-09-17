7bbf0939de93741a7303ba57672fd31ce5f7d2b5
spacemonkey1101 <ritambh54@gmail.com>
2023-09-17 15:14:44 +0530

    add function to verify plaintext password and hashed password from DB

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,10 @@
 from passlib.context import CryptContext
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash(password: str):
     # we need to create the hash of the password before creating the user
-    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     return pwd_context.hash(password)
+
+def verify_hashed_password(plain_pass , hashed_pass):
+    return pwd_context.verify(plain_pass,hashed_pass)

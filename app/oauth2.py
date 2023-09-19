f6a7ca43ac3dfaeeb411fb0b5faa03fbead4126c
spacemonkey1101 <ritambh54@gmail.com>
2023-09-19 20:31:05 +0530

    create get_current_user | with token as arg which depends on a oauth2_scheme defined above

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,7 +1,10 @@
+from fastapi import Depends, security
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas 
+from . import schemas
 
+# provide the login endpoint
+oauth2_scheme = security.OAuth2PasswordBearer(tokenUrl="login")
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -28,11 +31,15 @@ def verify_access_token(token: str, credentials_exception):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         # from payload we get the user_id that we put in while sending a payload
         user_id = payload.get("user_id")
-        
+
         if user_id is None:
             raise credentials_exception
-        
+
         token_data = schemas.TokenData(user_id=user_id)
     except JWTError:
         raise credentials_exception
     return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    pass
